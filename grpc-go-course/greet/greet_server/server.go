613bbabd7ee4ebc176a26f4d4731af8e0844bb5f
genkilife <wangyenkai@gmail.com>
2020-11-01 23:09:43 -0600

    add server code

--- grpc-go-course/greet/greet_server/server.go
+++ grpc-go-course/greet/greet_server/server.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/genkilife/learn_golang/grpc-go-course/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
 
 func main() {
 	fmt.Println("vim-go")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
